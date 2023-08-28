a38fac0f1f344fae811529e2977ceea4453f4fb4
Felix Brüggemann <Felix.brgm@icloud.com>
2023-08-28 22:24:28 +0200

    flask8 ex08

--- source/0/ex08/Loading.py
+++ source/0/ex08/Loading.py
@@ -3,23 +3,25 @@
 # if the terminal is smaller than this value you will have weird behaviour
 DEFAULT_TERMINAL_WIDTH = 50
 
+
 def ft_tqdm(lst: range) -> None:
-    lst = range(len(lst)) # Normalises negative ranges
+    lst = range(len(lst))  # Normalises negative ranges
 
     length = len(lst)
-    lengt_digits = len(str(length))
     for e in lst:
         percentage = int(100 * ((e+1)/(length)))
         text = "{:3}%|".format(percentage)
-        
-        loading_bar_width_filled = int((DEFAULT_TERMINAL_WIDTH * percentage/100))
+
+        loading_bar_width_filled = \
+            int((DEFAULT_TERMINAL_WIDTH * percentage/100))
         text += '█' * loading_bar_width_filled
 
-        loading_bar_width_empty = DEFAULT_TERMINAL_WIDTH - loading_bar_width_filled
-        text += ' ' *  loading_bar_width_empty
-        
+        loading_bar_width_empty = \
+            DEFAULT_TERMINAL_WIDTH - loading_bar_width_filled
+        text += ' ' * loading_bar_width_empty
+
         text += '| '
-        text += "{:3}".format(e + 1)
+        text += "{:len(str(length))}".format(e + 1)
         text += "/"
         text += str(len(lst))
         print(text)
